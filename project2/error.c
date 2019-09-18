280754b35d90483de05a8fd6b2a9edc6a4f9b1be
gequalspisquared <55361906+gequalspisquared@users.noreply.github.com>
2019-09-18 15:48:50 -0400

    Update error.c

--- project2/error.c
+++ project2/error.c
@@ -4,6 +4,7 @@
 #include <math.h>
 /*#define double float
 #define sin sinf*/
+#define double long double
 // Allows for switching between using floats and doubles easily
 
 // Returns the absolute value of a function
@@ -38,7 +39,7 @@ double trapIntegrate(double (*function)(double), double leftBound, double rightB
 	double ans=0,var,stepSize=(rightBound-leftBound)/binNum;
 	ans += function(leftBound);
 
-	for(var = leftBound+stepSize; var < rightBound; var += stepSize) {
+	for(var = leftBound+stepSize; var < rightBound-(stepSize/2); var += stepSize) {
 		ans += 2*function(var);
 	}
 
@@ -46,18 +47,28 @@ double trapIntegrate(double (*function)(double), double leftBound, double rightB
 	return ans*(rightBound-leftBound)/(2*binNum);
 }
 
+double simpIntegrate(double (*function)(double), double leftBound, double rightBound, int binNum) {
+	return (1.0/3)*(trapIntegrate(function,leftBound,rightBound,binNum)) + (2.0/3)*(midpIntegrate(function,leftBound,rightBound,binNum));
+}
+
 // This is the function we are analyzing
 double sine(double x) {
 	return sin(x);
+}
+
+double sqroot(double x) {
+	return sqrt(x);
 }
 
 void main() {
 	int binSize;
 
 	for(binSize = 2; binSize < 21000000; binSize *= 10) {
-		// printf("%8d %6f\n",binSize,dabs(1.416147 - leftIntegrate(sine,0,2,binSize)));
-		printf("%8d %6f\n",binSize,dabs(1.416147 - midpIntegrate(sine,0,2,binSize)));
-		// printf("%8d %6f\n",binSize,dabs(1.416147 - trapIntegrate(sine,0,2,binSize)));
+		// printf("%8d %Le\n",binSize,dabs(cos(2) - 1 + leftIntegrate(sine,0,2,binSize)));
+		// printf("%8d %Le\n",binSize,dabs(cos(2) - 1 + midpIntegrate(sine,0,2,binSize)));
+		printf("%8d %Le\n",binSize,dabs(cos(2) - 1 + simpIntegrate(sine,0,2,binSize)));
+		// printf("%8d %Le\n",binSize,dabs(cos(2) - 1 + trapIntegrate(sine,0,2,binSize)));
+		// printf("%8d %Le\n",binSize,dabs((2.0/3)*pow(2,1.5) - 2.0/3 - midpIntegrate(sqroot,1,2,binSize)));
 		// printf("%8d %6f %6f\n",binSize,dabs(1.416147 - leftIntegrate(sine,0,2,binSize)),binSize,dabs(1.416147 - midpIntegrate(sine,0,2,binSize)));
 	}
 }
