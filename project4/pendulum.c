41c80710a3a41cfe2f2b08a44e876c62a84b316b
gequalspisquared <55361906+gequalspisquared@users.noreply.github.com>
2019-10-06 13:11:04 -0400

    Create pendulum.c

---
+++ project4/pendulum.c
@@ -0,0 +1,91 @@
+/* Simulates a pendulum and demonstrates the amplitude does affect the period */
+#include <stdio.h>
+#include <math.h>
+#define  double   long double
+
+/* Set up general parameters */
+static double g  = 9.81;
+static double r  = 1;
+static double L  = 10;
+static double dt = 1e-6;
+
+/* Returns the absolute value of a double */
+int dabs(double x) {
+	if (x < 0)
+		return 0;
+	return 1;
+}
+
+/* Returns the angular acceleration of the pendulum */
+double alpha(double theta) {return -g/L*sin(theta);}
+
+void main(int argc, char* argv[]) {
+	double theta = M_PI/144; /* initial theta, must be in radians */
+	double omega = 0;
+	double t = 0;
+	double t1 = 0, t2 = 0;
+	double x, y;
+	int sign ;
+
+	while(t < 1e1) { /* loops through the simulation */
+		if (*argv[1] == '1') { /* this section is just used for anim */
+			x = L*sin(theta);
+			y = -L*cos(theta);
+
+			printf("l3 0 0 0 %Le %Le 0\n", x, y);
+			printf("c3 %Le %Le  0 %Le\n", x, y, r);
+			printf("F\n");
+		}
+
+		else if (*argv[1] == '2') { /* print theta as a function of time */
+			printf("%Le %Le\n", t, theta);
+		}
+
+		else if (*argv[1] == '3') { /* print omega as a function of time */
+			printf("%Le %Le\n", t, omega);
+		}
+
+		else {}
+
+		sign = dabs(omega);
+		theta += omega*dt;
+		omega += alpha(theta)*dt;
+		t += dt;
+
+		if (dabs(omega) != sign) {
+			if (t == dt)
+				continue;
+			printf("dabs(omega) = %d, sign = %d\n",dabs(omega),sign);
+
+			if (t1 < dt) {
+				printf("t1 = %Le\n",t);
+				t1 = t;
+			}
+			else if (t2 < dt) {
+				printf("t2 = %Le\n",t);
+				t2 = t;
+			}
+		}
+	}
+
+	if (*argv[1] == '1') { /* this section is just used for anim */
+		x = L*sin(theta);
+		y = -L*cos(theta);
+
+		printf("l3 0 0 0 %Le %Le 0\n", x, y);
+		printf("c3 %Le %Le  0 %Le\n", x, y, r);
+		printf("F\n");
+	}
+
+	else if (*argv[1] == '2') { /* print theta as a function of time */
+		printf("%Le %Le\n", t, theta);
+	}
+
+	else if (*argv[1] == '3') { /* print omega as a function of time */
+		printf("%Le %Le\n", t, omega);
+	}
+
+	else {}
+
+	printf("%Le %Le %Le\n", t1, t2, (t2-t1)*2);
+}
