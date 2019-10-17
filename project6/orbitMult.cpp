2abe7e5b5735197449a052390d9a7a00bbed7b0d
nicholas <nickcrusher30@gmail.com>
2019-10-17 10:30:15 -0400

    some changes

---
+++ project6/orbitMult.cpp
@@ -0,0 +1,82 @@
+/*  Simulates celestial bodies in motion  */
+/*  Written by Nicholas Crane
+	Vector library made with assistance from Jim Doxtader  */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "../vec3.hpp" 
+
+#define double long double
+#define NUMBODIES 100
+
+static double G = 4*M_PI*M_PI;
+
+class Body 
+{
+	public:
+		double m,r;
+		vec3d s,v;
+		Body() {s = vec3d(1,0,0);v = vec3d(0,2*M_PI,0);m = 1e-6;r = 1e-1;}
+		Body(vec3d pos, vec3d vel, double mass, double radius)
+		{
+			s = pos; 
+			v = vel;
+			m = mass;
+			r = radius;
+		}
+};
+
+vec3d forceG(double m1, double m2, vec3d pos1, vec3d pos2)
+{
+	return (pos1-pos2)*(-G*m1*m2/pow((pos1-pos2).mag(),3));
+}
+
+int main() 
+{
+	double t = 0;
+	double dt = 1e-2;
+	int i,j;
+	vec3d force;
+
+	Body body[NUMBODIES];
+	body[0] = Body(vec3d(0,0,0),vec3d(0,0,0),1,0.25);
+	body[1] = Body(vec3d(1,0,0),vec3d(0,2.5*M_PI,0),3.3e-5,0.05);
+	for(i = 2; i < NUMBODIES; i++)
+	{
+		body[i] = Body(vec3d((drand48()-.5)*5,(drand48()-0.5)*5,(drand48()-0.5)/10),vec3d(drand48()*2*M_PI,drand48()*2*M_PI,drand48()/3),1e-6,0.05);
+	}
+
+	while (true)
+	{
+		for(i = 0; i < NUMBODIES; i++)
+		{
+			body[i].s += body[i].v*(dt/2);
+		}
+
+		for(i = 0; i < NUMBODIES; i++)
+		{
+			for(j = (i+1); j < NUMBODIES; j++)
+			{
+				force = forceG(body[i].m,body[j].m,body[i].s,body[j].s);
+				body[i].v += force*(dt/body[i].m);
+				body[j].v += force*(-dt/body[j].m);
+			}
+		}
+
+		for(i = 0; i < NUMBODIES; i++)
+		{
+			body[i].s += body[i].v*(dt/2);
+		}
+
+		for(i = 0; i < NUMBODIES; i++)
+		{
+			printf("c3 %Le %Le %Le %Le\n",body[i].s.get_i(),body[i].s.get_j(),body[i].s.get_k(),body[i].r);
+		}
+
+		printf("T -0.9 0.8\n");
+		printf("E = %.15Le\n",(body[1].v.mag()*body[1].v.mag())*0.5 - G/(body[1].s-body[0].s).mag());
+		printf("T -0.9 0.7\n");
+		printf("r = %.15Le\n",(body[1].s-body[0].s).mag());
+		printf("F\n");
+	}
+}
---
+++ vec3.hpp
@@ -0,0 +1,79 @@
+/* 
+Uses classes to create a vector math library
+Written by Nicholas Crane
+Made with assistance from Jim Doxtader 
+*/
+
+
+#include <math.h>
+#define double long double
+
+class vec3d
+{
+	public:
+		vec3d() {i,j,k = 0;}
+		vec3d(double x, double y, double z) {i = x; j = y; k = z;}
+
+		~vec3d() {}
+
+		double get_i() {return i;}
+		double get_j() {return j;}
+		double get_k() {return k;} 
+
+		inline vec3d operator+(vec3d vector)
+		{
+			double x = i + vector.get_i();
+			double y = j + vector.get_j();
+			double z = k + vector.get_k();
+			return vec3d(x,y,z);
+		}
+
+		inline void operator+=(vec3d vector)
+		{
+			i += vector.get_i();
+			j += vector.get_j();
+			k += vector.get_k();
+		}
+
+		inline vec3d operator-(vec3d vector)
+		{
+			double x = i - vector.get_i();
+			double y = j - vector.get_j();
+			double z = k - vector.get_k();
+			return vec3d(x,y,z);
+		}
+
+		inline vec3d operator*(double scalar)
+		{
+			return vec3d(scalar*i,scalar*j,scalar*k);
+		}
+
+		inline vec3d operator*(vec3d vector)
+		{
+			return vec3d(vector.get_i()*i,vector.get_j()*j,vector.get_k()*k);
+		}
+
+		inline double mag()
+		{
+			return sqrt(i*i+j*j+k*k);
+		}
+
+		double dot(vec3d vec) 
+		{
+			return i*vec.get_i()+
+				   j*vec.get_j()+
+				   k*vec.get_k();
+		}
+
+		vec3d cross(vec3d vec)
+		{
+			double x,y,z;
+        	x = (j*vec.get_k())-(k*vec.get_j());
+        	y = (k*vec.get_i())-(i*vec.get_k());
+			z = (i*vec.get_j())-(j*vec.get_i());
+			return vec3d(x,y,z);
+		}
+
+	private:
+		double i,j,k;
+};
