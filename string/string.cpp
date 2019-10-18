eec1bfb19d0cc92594a93fef873f11f97beca3ba
nicholas <nickcrusher30@gmail.com>
2019-10-17 23:39:08 -0400

    updates

--- string/string.cpp
+++ string/string.cpp
@@ -8,11 +8,15 @@ Written by Nicholas Crane
 
 #define double long double
 
-static int N = 3;
-static double mu = 0.1;
-static double Lo = 1;
+/* Parameters affecting the string */
+static int N = 1001; // the number of nodes
+static double mu = 0.1; // the linear mass density of the string
+static double Lo = 1; // the length of the unstretched string
 static double k = 1000; // k is equal to the stiffness alpha
+static double T = 1000; // initial tension along the string
+static double r = 5e-3; // the radius of each node
 
+/* A class containing the info for each node */
 class Node 
 {
     public:
@@ -26,15 +30,58 @@ class Node
         ~Node() {}
 };
 
-vec3d springForce(vec3d pos1, vec3d pos, vec3d pos2)
+/* The equation for the force experienced by each node */
+vec3d springForce(vec3d pos1, vec3d pos, vec3d pos2, double ro)
 {
-    return ((pos-pos1)*(1/(pos-pos1).mag())*(pos-pos1) + (pos-pos2)*(1/(pos-pos2).mag())*(pos-pos2))*k;
+    return ( ((pos-pos1)*(1/(pos-pos1).mag())) * ((pos-pos1).mag() - ro) + ((pos-pos2)*(1/(pos-pos2).mag())) * ((pos-pos2).mag() - ro) )*-k;
 }
 
 int main()
 {
     double m = mu*Lo/N;
     double ro = Lo/(N-1);
+    double L = (T/k) + Lo;
+    float dt = 0.0001;
+    float t = 0;
+    int i,j;
 
+    Node node[N];
+    for(i = 0; i < N; i++)
+    {
+        node[i] = Node(vec3d(i*(L/(N-1.0)) - L/2,0,0),vec3d(0,0,0));
+        if (i == ((N+1)/2)-1)
+        {
+            node[i] = Node(vec3d(i*(L/(N-1.0)) - L/2,0.1,0),vec3d(0,0,0));
+        }
+    }
+
+    // while(t < 0.1)
+    while(true)
+    {
+		for(i = 0; i < (N-1); i++) // update the position of each node 
+		{
+			node[i].s += node[i].v*(dt/2);
+		}
+
+		for(i = 1; i < (N-1); i++) // update the velocity of each node
+		{
+			node[i].v += springForce(node[i-1].s,node[i].s,node[i+1].s,ro)*(dt/m);
+		}
+
+		for(i = 0; i < (N-1); i++) // update the position of each node again
+		{
+			node[i].s += node[i].v*(dt/2);
+		}
+
+
+        t += dt;
+        for(i = 0; i < N; i++)
+        {
+            printf("c3 %Le %Le %Le %Le\n",node[i].s.get_i(),node[i].s.get_j(),node[i].s.get_k(),r);
+        }
+
+        // printf("!%Le\n",node[1].v.get_j());
+        printf("F\n");
+    }
 
 }
